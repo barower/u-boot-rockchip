2989be138d946518ace66032f0904d35338e45e9
Jonas Karlman <jonas@kwiboo.se>
2018-08-05 20:58:54 +0200

    rk3288-miqi: get ethaddr from efuse

--- board/mqmaker/miqi_rk3288/miqi-rk3288.c
+++ board/mqmaker/miqi_rk3288/miqi-rk3288.c
@@ -6,3 +6,46 @@
 
 #include <common.h>
 #include <spl.h>
+#include <hash.h>
+#include <u-boot/sha256.h>
+
+static void setup_macaddr(void)
+{
+#if CONFIG_IS_ENABLED(CMD_NET)
+	int ret;
+	const char *cpuid = env_get("cpuid#");
+	u8 hash[SHA256_SUM_LEN];
+	int size = sizeof(hash);
+	u8 mac_addr[6];
+
+	/* Only generate a MAC address, if none is set in the environment */
+	if (env_get("ethaddr"))
+		return;
+
+	if (!cpuid) {
+		debug("%s: could not retrieve 'cpuid#'\n", __func__);
+		return;
+	}
+
+	ret = hash_block("sha256", (void *)cpuid, strlen(cpuid), hash, &size);
+	if (ret) {
+		debug("%s: failed to calculate SHA256\n", __func__);
+		return;
+	}
+
+	/* Copy 6 bytes of the hash to base the MAC address on */
+	memcpy(mac_addr, hash, 6);
+
+	/* Make this a valid MAC address and set it */
+	mac_addr[0] &= 0xfe;  /* clear multicast bit */
+	mac_addr[0] |= 0x02;  /* set local assignment bit (IEEE802) */
+	eth_env_set_enetaddr("ethaddr", mac_addr);
+#endif
+}
+
+int rk3288_board_late_init(void)
+{
+	setup_macaddr();
+
+	return 0;
+}
