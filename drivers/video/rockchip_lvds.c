5b7b9150321d51c79dd6bdc147ab33da53a1de93
Wyon Bi <bivvy.bi@rock-chips.com>
2019-11-07 10:10:47 +0800

    video: rockchip_lvds: check PLL lock state before enable tx
    Fixes: 264238023153 ("video: rockchip: lvds: check PLL lock state before enable tx")
    Change-Id: I318019da424f2785e6e0432d5646e57f002986b3
    Signed-off-by: Wyon Bi <bivvy.bi@rock-chips.com>

--- drivers/video/rockchip_lvds.c
+++ drivers/video/rockchip_lvds.c
@@ -478,6 +478,8 @@ static void rk3288_output_lvds(struct display_state *state)
 {
 	struct connector_state *conn_state = &state->conn_state;
 	struct rockchip_lvds_device *lvds = conn_state->private;
+	u32 status;
+	int ret;
 
 	rk3288_lvds_pwr_on(state);
 
@@ -515,6 +517,13 @@ static void rk3288_output_lvds(struct display_state *state)
 		    RK3288_LVDS_CH0_REG20_LSB);
 
 	lvds_writel(lvds, RK3288_LVDS_CFG_REGC, RK3288_LVDS_CFG_REGC_PLL_ENABLE);
+	ret = readl_poll_timeout(lvds->regbase + RK3288_LVDS_CH0_REGF, status,
+				 status & RK3288_LVDS_CH0_PLL_LOCK, 10000);
+	if (ret) {
+		printf("%s: PLL is not lock\n", __func__);
+		return;
+	}
+
 	lvds_writel(lvds, RK3288_LVDS_CFG_REG21, RK3288_LVDS_CFG_REG21_TX_ENABLE);
 }
 
