1e5c1e9b6c1d0f66b4d5b1f622115a1fc94d51d6
Eugen Hristev <eugen.hristev@collabora.com>
2023-04-19 16:45:25 +0300

    regulator: rename dev_pdata to plat
    Simplify the subsystem by renaming `dev_pdata` to just `plat`.
    No functional change, just trivial renaming.
    
    Suggested-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Eugen Hristev <eugen.hristev@collabora.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>

--- drivers/power/regulator/fixed.c
+++ drivers/power/regulator/fixed.c
@@ -24,10 +24,10 @@ struct fixed_clock_regulator_plat {
 static int fixed_regulator_of_to_plat(struct udevice *dev)
 {
 	struct dm_regulator_uclass_plat *uc_pdata;
-	struct regulator_common_plat *dev_pdata;
+	struct regulator_common_plat *plat;
 	bool gpios;
 
-	dev_pdata = dev_get_plat(dev);
+	plat = dev_get_plat(dev);
 	uc_pdata = dev_get_uclass_plat(dev);
 	if (!uc_pdata)
 		return -ENXIO;
@@ -35,7 +35,7 @@ static int fixed_regulator_of_to_plat(struct udevice *dev)
 	uc_pdata->type = REGULATOR_TYPE_FIXED;
 
 	gpios = dev_read_bool(dev, "gpios");
-	return regulator_common_of_to_plat(dev, dev_pdata,
+	return regulator_common_of_to_plat(dev, plat,
 					   gpios ? "gpios" : "gpio");
 }
 
@@ -91,7 +91,7 @@ static int fixed_clock_regulator_get_enable(struct udevice *dev)
 static int fixed_clock_regulator_set_enable(struct udevice *dev, bool enable)
 {
 	struct fixed_clock_regulator_plat *priv = dev_get_priv(dev);
-	struct regulator_common_plat *dev_pdata = dev_get_plat(dev);
+	struct regulator_common_plat *plat = dev_get_plat(dev);
 	int ret = 0;
 
 	if (enable) {
@@ -104,11 +104,11 @@ static int fixed_clock_regulator_set_enable(struct udevice *dev, bool enable)
 	if (ret)
 		return ret;
 
-	if (enable && dev_pdata->startup_delay_us)
-		udelay(dev_pdata->startup_delay_us);
+	if (enable && plat->startup_delay_us)
+		udelay(plat->startup_delay_us);
 
-	if (!enable && dev_pdata->off_on_delay_us)
-		udelay(dev_pdata->off_on_delay_us);
+	if (!enable && plat->off_on_delay_us)
+		udelay(plat->off_on_delay_us);
 
 	return ret;
 }
