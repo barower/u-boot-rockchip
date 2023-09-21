1e7384d3a45ac10b6c0d162e819168676e2b6dc9
Jonas Karlman <jonas@kwiboo.se>
2023-09-21 06:48:17 +0000

    WIP: load and run bl30.bin and bl301.bin
    Put bl30.bin and bl301.bin in root of U-Boot folder when building.

--- arch/arm/mach-meson/spl.c
+++ arch/arm/mach-meson/spl.c
@@ -88,6 +88,129 @@ __weak void spl_board_prepare_for_boot(void)
 	}
 }
 
+#if CONFIG_IS_ENABLED(FIT_IMAGE_POST_PROCESS)
+
+#define     AO_SEC_SD_CFG15                                    (0xc8100000 + (0x8f << 2))
+#define SEC_HIU_MAILBOX_SET_0                                  (0xda83c400 + (0x01 << 2))
+#define SEC_HIU_MAILBOX_STAT_0                                 (0xda83c400 + (0x02 << 2))
+#define SEC_HIU_MAILBOX_CLR_0                                  (0xda83c400 + (0x03 << 2))
+
+#define MB_SRAM_BASE 0xd9013800
+
+#define CMD_SHA         0xc0de0001
+#define CMD_OP_SHA      0xc0de0002
+#define CMD_DATA_LEN    0xc0dec0d0
+#define CMD_DATA        0xc0dec0de
+#define CMD_END         0xe00de00d
+
+static void mb_send_data(uint32_t val, uint32_t port)
+{
+	unsigned long  base_addr = SEC_HIU_MAILBOX_SET_0;
+	unsigned long  set_addr;
+
+	if (port > 5) {
+		printf("Error: Use the error port num!\n");
+		return;
+	}
+
+	set_addr = base_addr + port*3*4;
+
+	if (!val) {
+		printf("Error: mailbox try to send zero val!\n");
+		return;
+	}
+
+	writel(val, set_addr);
+
+	return;
+}
+
+static uint32_t mb_read_data(uint32_t port)
+{
+	unsigned long base_addr = SEC_HIU_MAILBOX_STAT_0;
+	uint32_t val;
+
+	if (port > 5) {
+		printf("Error: Use the error port num!\n");
+		return 0;
+	}
+
+	val = readl(base_addr + port*3*4);
+
+	if (val)
+		return val;
+	else {
+//		print_out("Warning: read mailbox val=0.\n");
+		return 0;
+	}
+}
+
+static void send_bl30x(void *addr, size_t size, const uint8_t * sha2, \
+	uint32_t sha2_length, const char * name)
+{
+	int i;
+	*(unsigned int *)MB_SRAM_BASE = size;
+
+	if (0 == strcmp("bl301", name)) {
+		/*bl301 must wait bl30 run*/
+		printf("Wait bl30...");
+		while (0x3 != ((readl(AO_SEC_SD_CFG15) >> 20) & 0x3)) {}
+		printf("Done\n");
+	}
+
+	printf("Sending ");
+	printf(name);
+	//printf("time=0x%x size=0x%x\n", readl(0xc1109988),size);
+
+	mb_send_data(CMD_DATA_LEN, 3);
+	do {} while(mb_read_data(3));
+	memcpy((void *)MB_SRAM_BASE, (const void *)sha2, sha2_length);
+	mb_send_data(CMD_SHA, 3);
+	do {} while(mb_read_data(3));
+
+	for (i = 0; i < size; i+=1024) {
+		printf(".");
+		if (size >= i + 1024)
+			memcpy((void *)MB_SRAM_BASE,(const void *)(unsigned long)(addr+i),1024);
+		else if(size > i)
+			memcpy((void *)MB_SRAM_BASE,(const void *)(unsigned long)(addr+i),(size-i));
+
+		mb_send_data(CMD_DATA, 3);
+		do {} while(mb_read_data(3));
+	}
+	mb_send_data(CMD_OP_SHA, 3);
+
+	do {} while(mb_read_data(3));
+	printf("OK. \nRun ");
+	printf(name);
+	printf("...\n");
+
+	/* The BL31 will run after this command */
+	mb_send_data(CMD_END,3);//code transfer end.
+}
+
+void board_fit_image_post_process(const void *fit, int node, void **p_image, size_t *p_size)
+{
+	const char *name = fit_get_name(fit, node, NULL);
+	int noffset = 0, value_len;
+	uint8_t *value;
+
+	if (strcmp("bl30", name) && strcmp("bl301", name))
+		return;
+
+	fdt_for_each_subnode(noffset, fit, node) {
+		if (strncmp(fit_get_name(fit, noffset, NULL), FIT_HASH_NODENAME, strlen(FIT_HASH_NODENAME)))
+			continue;
+
+		if (fit_image_hash_get_value(fit, noffset, &value, &value_len))
+			continue;
+
+		send_bl30x(*p_image, *p_size, value, value_len, name);
+		break;
+	}
+}
+#endif
+
 static int pwm_voltage_table[][2] = {
 	{ 0x1c0000,  860},
 	{ 0x1b0001,  870},
